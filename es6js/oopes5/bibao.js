52af682e8534da6784fb3b475f9c5cb7a9dd3607
wangxin <2467752800@qq.com>
2019-02-18 10:53:46 +0800

    使用vscode提交

---
+++ es6js/oopes5/bibao.js
@@ -0,0 +1,37 @@
+class Test {
+    constructor (a, b) {
+        this.name = a
+        this.age = b
+        this.handle = this.handle.bind(this)
+    }
+    pointOut () {
+        console.log(this.name)
+    }
+    handle () {
+        this.pointOut ()
+    }
+    returnHandle () {
+        return () => {
+            this.pointOut()
+        }
+    }
+}
+class AnotherTest extends Test {
+    constructor (a,last) {
+        this.last = last
+
+        super(a)
+        // this.last = last
+    }
+    pointOutAll () {
+        console.log(this.name + this.last)
+    }
+}
+// let one = new Test('battle')
+let two = new AnotherTest('battle', 'forg')
+// one.pointOut()
+// one.handle()
+// one.returnHandle()()
+let testFun = two.handle
+// two.handle()
+testFun()
