0da835fd610543a52ed3812cc487f7a5e4c6b11a
wangxin <2467752800@qq.com>
2019-03-05 11:36:21 +0800

    async 的两种错误处理机制

--- es6js/promise/genertor.js
+++ es6js/promise/genertor.js
@@ -12,27 +12,39 @@ async function timeout1(ms) {
 }
 
 async function asyncPrint(value, ms, next) {
-  var start = new Date().getTime()
-  await timeout(ms);
-  console.log('hi')
-  await timeout1(next);
-  console.log(value);
-  var end = new Date().getTime()
-  console.log(end - start)
+  // console.time('go')
+  // await timeout(ms)
+  // await timeout1(next)
+  // console.timeEnd('go')
+
+  console.time('next')
+  var test = false
+  if (test) {
+    const t1 = timeout(ms)
+    const t2 = timeout1(next)
+    await t1
+    await t2
+  } else {
+    await timeout(ms)
+    await timeout1(next)
+  }
+  console.timeEnd('next')
 }
 
 // asyncPrint('hello world', 1000, 3000);
 
 async function readFiles(url) {
-  await new Promise((resolve) => {
+  // await new Promise((resolve) => { 普通写法
+  return await new Promise((resolve, reject) => {
     fs.readFile(url, {
       encoding: 'utf-8'
     }, (err, content) => {
       if (err) {
-        console.log(err)
+        // console.log(err)
+        reject(err)
       }
-      console.log(content)
-      resolve(content)
+      // console.log(content)
+      resolve({res:content, err: 'ok'})
     })
   })
 }
@@ -49,12 +61,19 @@ function readFiles1(url) {
 
 }
 async function asyncRead(url) {
-  await readFiles(url)
-  readFiles1(url)
-  // .then(content => {
-  //   console.log(content)
-  // })
-  console.log('读取文件完成')
+  // try {
+  //   let response = await readFiles(url)
+  //   console.log(response)
+  // } catch (error) {
+  //   console.error(error, '捕捉到了错误')
+  // }
+  let [err, data] = await readFiles(url).then(data => {
+    return [null, data]
+  }).catch(err => {
+    return [err, null]
+  })
+  console.log(data, '成功')
+  console.log(err, '失败')
 }
 
 asyncRead('../serve.js')
